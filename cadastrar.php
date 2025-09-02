35b66e9365dfb677a19d25fd1c34314b4092a3f9
Jocerlan Andrade <jocerlan2@gmail.com>
2025-09-02 14:35:13 -0300

    commit 9f1e2b3d4c5e6f7g8h9i0j1k2l3m4n5o6p7q8r9s Author: Joce <

--- cadastrar.php
+++ cadastrar.php
@@ -1,4 +1,5 @@
 <?php
+ob_start(); // Inicia o buffer de saída
 include ("conexao.php");
 
 $nome=$_POST['nome'];
@@ -8,5 +9,7 @@ $resenha=$_POST['resenha'];
 $sql= "INSERT INTO livro(nome,autor,resenha) VALUES ('$nome','$autor','$resenha')";
 
 $executa = mysqli_query($con,$sql);
-header("Location: listar.php");
+ob_end_clean(); // Limpa o buffer de saída
+header("Location:./listar.php");
+exit(); // Garante que o script pare após o redirecionamento
 ?>
